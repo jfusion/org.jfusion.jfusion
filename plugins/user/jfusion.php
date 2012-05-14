ee4387f12ad1f9af6db53f085ae37126aaf00984
Morten Hundevad <fannoj@gmail.com>
2012-05-14 16:27:25 +0200

    fixed/add send of emails if users are created from admin panel.

--- plugins/user/jfusion.php
+++ plugins/user/jfusion.php
@@ -765,7 +765,81 @@ class plgUserJfusion extends JPlugin
  	    $result = $this->onBeforeStoreUser($user, $isnew, $new);
  	    return $result;
 	}
-	public function onUserAfterSave($user, $isnew, $success, $msg){
+	public function onUserAfterSave($user, $isnew, $success, $msg) {
+	        if (!JPluginHelper::isEnabled('user','joomla')) {
+            $user = JFactory::getUser();
+            $levels = implode(',', $user->getAuthorisedViewLevels());
+            
+            $db = JFactory::getDbo();
+            $query = $db->getQuery(true);
+            
+            $query->select('folder, type, element, params')
+            ->from('#__extensions')
+            ->where('type =' . $db->Quote('plugin'))
+            ->where('element =' . $db->Quote('joomla'))
+            ->where('folder =' . $db->Quote('user'))
+            ->where('access IN (' . $levels . ')');
+                
+            $plugin = $db->setQuery($query,0,1)->loadObject();
+                
+            $params = new JRegistry;
+            $params->loadString($plugin->params);
+            
+            // Initialise variables.
+            $app    = JFactory::getApplication();
+            $config = JFactory::getConfig();
+            $mail_to_user = $params->get('mail_to_user', 1);
+
+            if ($isnew) {
+                // TODO: Suck in the frontend registration emails here as well. Job for a rainy day.
+            
+                if ($app->isAdmin()) {
+                    if ($mail_to_user) {
+            
+                        // Load user_joomla plugin language (not done automatically).
+                        $lang = JFactory::getLanguage();
+                        $lang->load('plg_user_joomla', JPATH_ADMINISTRATOR);
+            
+                        // Compute the mail subject.
+                        $emailSubject = JText::sprintf(
+                                'PLG_USER_JOOMLA_NEW_USER_EMAIL_SUBJECT',
+                                $user['name'],
+                                $config->get('sitename')
+                        );
+            
+                        // Compute the mail body.
+                        $emailBody = JText::sprintf(
+                                'PLG_USER_JOOMLA_NEW_USER_EMAIL_BODY',
+                                $user['name'],
+                                $config->get('sitename'),
+                                JUri::root(),
+                                $user['username'],
+                                $user['password_clear']
+                        );
+            
+                        // Assemble the email data...the sexy way!
+                        $mail = JFactory::getMailer()
+                        ->setSender(
+                                array(
+                                        $config->get('mailfrom'),
+                                        $config->get('fromname')
+                                )
+                        )
+                        ->addRecipient($user['email'])
+                        ->setSubject($emailSubject)
+                        ->setBody($emailBody);
+            
+                        if (!$mail->Send()) {
+                            // TODO: Probably should raise a plugin error but this event is not error checked.
+                            JError::raiseWarning(500, JText::_('ERROR_SENDING_EMAIL'));
+                        }
+                    }
+                }
+            }
+            else {
+                // Existing user - nothing to do...yet.
+            }
+        }
  	    $result = $this->onAfterStoreUser($user, $isnew, $success, $msg);
  	    return $result;
 	}
