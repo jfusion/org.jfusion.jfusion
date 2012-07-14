f87e7845d744a54fd2333584846874577991663e
hwevers <support@breincoach.eu>
2012-07-14 17:08:00 +0200

    Wordpress get Usergroups: Array key in database depended on the database prefix. Go figure!

--- components/com_jfusion/plugins/wordpress/wordpresshelper.php
+++ components/com_jfusion/plugins/wordpress/wordpresshelper.php
@@ -35,7 +35,8 @@ class JFusionWordpressHelper {
 	
 	function getUsergroupListWP() {
 		$db = JFusionFactory::getDatabase($this->getJname());
-		$query = "SELECT option_value FROM #__options WHERE option_name = 'wp_user_roles'";
+		$database_prefix = $params->get('database_prefix');
+		$query = "SELECT option_value FROM #__options WHERE option_name = '".$database_prefix."user_roles'";
 		$db->setQuery($query);
 		$roles_ser = $db->loadResult();
 		$roles = unserialize($roles_ser);
@@ -79,7 +80,8 @@ class JFusionWordpressHelper {
 		static $allroles;
 		if (!isset($allroles)){
 			$db = JFusionFactory::getDatabase($this->getJname());
-			$query = "SELECT option_value FROM #__options WHERE option_name = 'wp_user_roles'";
+			$database_prefix = $params->get('database_prefix');
+			$query = "SELECT option_value FROM #__options WHERE option_name = '".$database_prefix."user_roles'";
 			$db->setQuery($query);
 			$roles_ser = $db->loadResult();
 			$allroles = unserialize($roles_ser);
