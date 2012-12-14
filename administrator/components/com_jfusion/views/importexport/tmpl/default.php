21a9dac110db02e71bf99423b09ed9a81cbdf931
jfusion <morten@jfusion.org>
2012-12-14 21:08:47 +0100

    missing translations added change layout for import export a bit to better seperate the two. new package.

--- administrator/components/com_jfusion/views/importexport/tmpl/default.php
+++ administrator/components/com_jfusion/views/importexport/tmpl/default.php
@@ -46,116 +46,150 @@ defined('_JEXEC') or die('Restricted access');
     }
 </script>
 
+<?php
+jimport('joomla.html.pane');
+$paneTabs = JPane::getInstance('tabs');
+?>
+
 <form method="post" action="index.php" name="adminForm" id="adminForm">
+	<?php
+	echo $paneTabs->startPane('jfusion_import_export');
+	echo $paneTabs->startPanel( JText::_('IMPORT'), 'IMPORT' );
+	?>
     <input type="hidden" name="option" value="com_jfusion" />
     <input type="hidden" name="task" value="" />
     <input type="hidden" name="action" value="" />
-
-    <input name="file" size="60" type="file">
-    <br/>
-    <table style="border: 0;">
-        <tr style="padding: 0;">
-            <td style="padding: 0; width: 150px;">
-                <?php echo JText::_('DATABASE_TYPE'); ?>
+    <table>
+        <tr>
+            <td>
+				<?php echo JText::_('IMPORT_XML_FILE'); ?>
             </td>
-            <td style="padding: 0;">
+            <td>
+                <input name="file" size="60" type="file">
+            </td>
+        </tr>
+        <tr>
+            <td>
+				<?php echo JText::_('DATABASE_TYPE'); ?>
+            </td>
+            <td>
                 <input name="database_type" id="database_type" value="" class="text_area" size="20" type="text">
             </td>
         </tr>
-        <tr style="padding: 0;">
-            <td style="padding: 0; width: 150px;">
-                <?php echo JText::_('DATABASE_HOST'); ?>
+        <tr>
+            <td>
+				<?php echo JText::_('DATABASE_HOST'); ?>
             </td>
-            <td style="padding: 0;">
+            <td>
                 <input name="database_host" id="database_host" value="" class="text_area" size="20" type="text">
             </td>
         </tr>
-        <tr style="padding: 0;">
-            <td style="padding: 0; width: 150px;">
-                <?php echo JText::_('DATABASE_NAME'); ?>
+        <tr>
+            <td>
+				<?php echo JText::_('DATABASE_NAME'); ?>
             </td>
-            <td style="padding: 0;">
+            <td>
                 <input name="database_name" id="database_name" value="" class="text_area" size="20" type="text">
             </td>
         </tr>
-        <tr style="padding: 0;">
-            <td style="padding: 0; width: 150px;">
-                <?php echo JText::_('DATABASE_USER'); ?>
+        <tr>
+            <td>
+				<?php echo JText::_('DATABASE_USER'); ?>
             </td>
-            <td style="padding: 0;">
+            <td>
                 <input name="database_user" id="database_user" value="" class="text_area" size="20" type="text">
             </td>
         </tr>
-        <tr style="padding: 0;">
-            <td style="padding: 0; width: 150px;">
-                <?php echo JText::_('DATABASE_PASSWORD'); ?>
+        <tr>
+            <td>
+				<?php echo JText::_('DATABASE_PASSWORD'); ?>
             </td>
-            <td style="padding: 0;">
+            <td>
                 <input name="database_password" id="database_password" value="" class="text_area" size="20" type="text">
             </td>
         </tr>
-        <tr style="padding: 0;">
-            <td style="padding: 0; width: 150px;">
-                <?php echo JText::_('DATABASE_PREFIX'); ?>
+        <tr>
+            <td>
+				<?php echo JText::_('DATABASE_PREFIX'); ?>
             </td>
-            <td style="padding: 0;">
+            <td>
                 <input name="database_prefix" id="database_prefix" value="" class="text_area" size="20" type="text">
             </td>
         </tr>
     </table>
-    <?php
-    if ( isset($this->xmlList->document) ) {
-        echo JText::_('IMPORT_FROM_SERVER');
-        ?>
-        <br/>
-        <input type=radio name="xmlname" value="" checked>
-        <?php echo JText::_('JNONE'); ?>
-        <br/>
-        <?php
-        /**
-        * @ignore
-        * @var $val JSimpleXMLElement
-        */
-        foreach ($this->xmlList->document->children() as $key => $val) {
-            $pluginName = $val->attributes('name');
-            if ($pluginName) {
-                $pluginVersion = $val->attributes('version')?$val->attributes('version'):JText::_('UNKNOWN');
-                $pluginDesc = $val->attributes('desc')?$val->attributes('desc'):JText::_('NONE');
-                $pluginCreator = $val->attributes('creator')?$val->attributes('creator'):JText::_('UNKNOWN');
-                ?>
+    <br>
+    <br>
+	<?php
+	if ( isset($this->xmlList->document) ) {
+		echo JText::_('IMPORT_FROM_SERVER');
+		?>
+        <table>
+            <tr>
+                <td>
+                    <input type=radio name="xmlname" value="" checked>
+                </td>
+                <td>
+					<?php echo JText::_('JNONE'); ?>
+                </td>
+            </tr>
+            <br/>
 
-                <script type="text/javascript">
-                    window.addEvent('domready',function() {
-                        $('plugin<?php echo $key;?>').addEvent('click', function(e) {
-                            doShowHide(this.id);
+			<?php
+			/**
+			 * @ignore
+			 * @var $val JSimpleXMLElement
+			 */
+			foreach ($this->xmlList->document->children() as $key => $val) {
+				$pluginName = $val->attributes('name');
+				if ($pluginName) {
+					$pluginVersion = $val->attributes('version')?$val->attributes('version'):JText::_('UNKNOWN');
+					$pluginDesc = $val->attributes('desc')?$val->attributes('desc'):JText::_('JNONE');
+					$pluginCreator = $val->attributes('creator')?$val->attributes('creator'):JText::_('UNKNOWN');
+					?>
+
+                    <script type="text/javascript">
+                        window.addEvent('domready',function() {
+                            $('plugin<?php echo $key;?>').addEvent('click', function(e) {
+                                doShowHide(this.id);
+                            });
                         });
-                    });
-                </script>
-
-                <input type=radio name="xmlname" value="<?php echo $pluginName; ?>">
-                    <?php echo ucfirst($pluginName); ?>
-                    <a href="javascript:void(0);" id="plugin<?php echo $key;?>">[+]</a>
-                    <div style="display:none;" id="xplugin<?php echo $key; ?>">
-                    <?php echo JText::_('VERSION').': '.$pluginVersion?>
-                    <br/>
-                    <?php echo JText::_('DESCRIPTION').': '.ucfirst($pluginDesc)?>
-                    <br/>
-                    <?php echo JText::_('CREATOR').': '.$pluginCreator; ?>
-                </div>
-                <br/>
-                <?php
-            }
-        }
-    } else {
-        echo JText::_('NO_CONECTION_TO_JFUSION_SERVER');
-    }
-    ?>
+                    </script>
+                    <tr>
+                        <td style="vertical-align:top">
+                            <input type=radio name="xmlname" value="<?php echo $pluginName; ?>">
+                        </td>
+                        <td>
+							<?php echo ucfirst($pluginName); ?>
+                            <a href="javascript:void(0);" id="plugin<?php echo $key;?>">[+]</a>
+                            <div style="display:none;" id="xplugin<?php echo $key; ?>">
+								<?php echo JText::_('VERSION').': '.$pluginVersion?>
+                                <br/>
+								<?php echo JText::_('DESCRIPTION').': '.ucfirst($pluginDesc)?>
+                                <br/>
+								<?php echo JText::_('CREATOR').': '.$pluginCreator; ?>
+                            </div>
+                        </td>
+                    </tr>
+					<?php
+				}
+			}
+			?>
+        </table>
+		<?php
+	} else {
+		echo JText::_('NO_CONECTION_TO_JFUSION_SERVER');
+	}
+	echo $paneTabs->endPanel();
+	echo $paneTabs->startPanel( JText::_('EXPORT'), 'EXPORT' );
+	?>
+	<?php echo JText::_('EXPORT_DATABASE_INFO_DESC'); ?>
     <br>
     <br>
-    <?php echo JText::_('EXPORT_DATABASE_INFO_DESC'); ?>
-    <br>
-    <br>
-    <input name="dbinfo" type="checkbox"> <?php echo JText::_('EXPORT_DATABASE_INFO'); ?>
+	<?php echo JText::_('EXPORT_DATABASE_INFO'); ?> <input name="dbinfo" type="checkbox"><br/>
 
     <input type="hidden" name="jname" value="<?php echo $this->jname; ?>"/>
+	<?php
+	echo $paneTabs->endPanel();
+	echo $paneTabs->endPane();
+	?>
 </form>
