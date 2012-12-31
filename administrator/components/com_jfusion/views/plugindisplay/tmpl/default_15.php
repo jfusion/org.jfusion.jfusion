11ed16722fa72ab481d53f29e3ecddb1731a6736
jfusion <morten@jfusion.org>
2012-12-31 17:01:42 +0100

    correct directory

--- administrator/components/com_jfusion/views/plugindisplay/tmpl/default_15.php
+++ administrator/components/com_jfusion/views/plugindisplay/tmpl/default_15.php
@@ -484,7 +484,7 @@ window.addEvent('domready',function() {
 			    			</label>
 		    			</td>
 		    			<td>
-                            <input type="text" id="install_directory" name="install_directory" class="input_box" size="150" value="<?php echo JPATH_COMPONENT_ADMINISTRATOR.DS.'packages'.DS.'jfusion_xxxxxxx.zip'; ?>" />
+                            <input type="text" id="install_directory" name="install_directory" class="input_box" size="150" value="" />
 						    <input type="submit"  value="<?php echo JText::_('INSTALL'); ?>"/>
 						    <div id="spinnerDIR">
 						    </div>
