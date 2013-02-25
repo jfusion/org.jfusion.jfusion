350d36a8c005d513e924c8bc2bc12e3e460a4bb3
jfusion <morten@jfusion.org>
2013-02-25 11:45:25 +0100

    fixed build error for windows

--- create_release.bat
+++ create_release.bat
@@ -1,6 +1,6 @@
 @echo off
 SET FULLPATH=%~dp0
-SET PLUGIN_DIR="%FULLPATH%\components\com_jfusion\plugins"
+SET PLUGIN_DIR="%FULLPATH%components\com_jfusion\plugins"
 setlocal enableextensions
 
 :START
@@ -57,7 +57,7 @@ goto end
 
 :CLEAR_PACKAGES
 echo Remove module and plugin packages
-del "%FULLPATH%\administrator\components\com_jfusion\packages\*.zip"
+del "%FULLPATH%administrator\components\com_jfusion\packages\*.zip"
 IF "%action%"=="5" goto end
 
 :CLEAR_MAIN
@@ -71,14 +71,14 @@ goto create_main
 goto end
 
 :CREATE_PACKAGES
-del "%FULLPATH%\administrator\components\com_jfusion\packages\*.zip"
+del "%FULLPATH%administrator\components\com_jfusion\packages\*.zip"
 
 echo Create the new packages for the plugins and module
 
-call :CreatePackage modules\mod_jfusion_activity\ administrator\components\com_jfusion\packages\jfusion_mod_activity.zip mod_jfusion_activity
-call :CreatePackage modules\mod_jfusion_login\ administrator\components\com_jfusion\packages\jfusion_mod_login.zip mod_jfusion_login
-call :CreatePackage modules\mod_jfusion_whosonline\ administrator\components\com_jfusion\packages\jfusion_mod_whosonline.zip mod_jfusion_whosonline
-call :CreatePackage modules\mod_jfusion_user_activity\ administrator\components\com_jfusion\packages\jfusion_mod_user_activity.zip mod_jfusion_user_activity
+call :CreatePackage modules\mod_jfusion_activity\* administrator\components\com_jfusion\packages\jfusion_mod_activity.zip mod_jfusion_activity
+call :CreatePackage modules\mod_jfusion_login\* administrator\components\com_jfusion\packages\jfusion_mod_login.zip mod_jfusion_login
+call :CreatePackage modules\mod_jfusion_whosonline\* administrator\components\com_jfusion\packages\jfusion_mod_whosonline.zip mod_jfusion_whosonline
+call :CreatePackage modules\mod_jfusion_user_activity\* administrator\components\com_jfusion\packages\jfusion_mod_user_activity.zip mod_jfusion_user_activity
 
 call :CreatePackage plugins\authentication\* administrator\components\com_jfusion\packages\jfusion_plugin_auth.zip
 call :CreatePackage plugins\user\* administrator\components\com_jfusion\packages\jfusion_plugin_user.zip
@@ -111,9 +111,9 @@ call :CreatePackage components\com_jfusion\plugins\wordpress pluginpackages\jfus
 
 echo "create the new packages for the Magento Integration"
 
-call :CreatePackage modules\mod_jfusion_magecart side_projects\magento\jfusion_mod_magecart.zip mod_jfusion_magecart
-call :CreatePackage modules\mod_jfusion_mageselectblock side_projects\magento\jfusion_mod_mageselectblock.zip mod_jfusion_mageselectblock
-call :CreatePackage modules\mod_jfusion_magecustomblock side_projects\magento\jfusion_mod_magecustomblock.zip mod_jfusion_magecustomblock
+call :CreatePackage modules\mod_jfusion_magecart\* side_projects\magento\jfusion_mod_magecart.zip mod_jfusion_magecart
+call :CreatePackage modules\mod_jfusion_mageselectblock\* side_projects\magento\jfusion_mod_mageselectblock.zip mod_jfusion_mageselectblock
+call :CreatePackage modules\mod_jfusion_magecustomblock\* side_projects\magento\jfusion_mod_magecustomblock.zip mod_jfusion_magecustomblock
 
 call :CreatePackage plugins\system\magelib.* side_projects\magento\jfusion_plugin_magelib.zip magelib
 
@@ -124,40 +124,40 @@ IF "%action%"=="2" goto end
 echo Prepare the files for packaging
 md tmp
 md tmp\admin
-c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%\administrator\components\com_jfusion\*.*" "%FULLPATH%\tmp\admin" > NUL
-c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%\pluginpackages\*.*" "%FULLPATH%\tmp\admin\packages\" > NUL
-del "%FULLPATH%\tmp\admin\jfusion.xml"
+c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%administrator\components\com_jfusion\*.*" "%FULLPATH%\tmp\admin" > NUL
+c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%pluginpackages\*.*" "%FULLPATH%tmp\admin\packages\" > NUL
+del "%FULLPATH%tmp\admin\jfusion.xml"
 
 md tmp\admin\languages
-c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%\administrator\language\en-GB\*.*" "%FULLPATH%\tmp\admin\languages\en-GB\" > NUL
+c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%administrator\language\en-GB\*.*" "%FULLPATH%tmp\admin\languages\en-GB\" > NUL
 
 md tmp\front
-c:\windows\system32\xcopy /E /C /V /Y /EXCLUDE:%FULLPATH%\exclude.txt "%FULLPATH%\components\com_jfusion\*.*" "%FULLPATH%\tmp\front" > NUL
+c:\windows\system32\xcopy /E /C /V /Y /EXCLUDE:%FULLPATH%exclude.txt "%FULLPATH%components\com_jfusion\*.*" "%FULLPATH%tmp\front" > NUL
 
 md tmp\front\languages
-c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%\language\en-GB\*.*" "%FULLPATH%\tmp\front\languages\en-GB\" > NUL
+c:\windows\system32\xcopy /E /C /V /Y "%FULLPATH%language\en-GB\*.*" "%FULLPATH%tmp\front\languages\en-GB\" > NUL
 
-copy "%FULLPATH%\administrator\components\com_jfusion\jfusion.xml" "%FULLPATH%\tmp" /V /Y > NUL
-copy "%FULLPATH%\administrator\components\com_jfusion\install.jfusion.php" "%FULLPATH%\tmp" /V /Y > NUL
-copy "%FULLPATH%\administrator\components\com_jfusion\uninstall.jfusion.php" "%FULLPATH%\tmp" /V /Y > NUL
+copy "%FULLPATH%administrator\components\com_jfusion\jfusion.xml" "%FULLPATH%tmp" /V /Y > NUL
+copy "%FULLPATH%administrator\components\com_jfusion\install.jfusion.php" "%FULLPATH%tmp" /V /Y > NUL
+copy "%FULLPATH%administrator\components\com_jfusion\uninstall.jfusion.php" "%FULLPATH%tmp" /V /Y > NUL
 
 echo Update the revision number
 
 echo Revision set to %REVISION%
 echo Timestamp set to %TIMESTAMP%
-call :CreateXml %FULLPATH%\tmp\jfusion
+call :CreateXml %FULLPATH%tmp\jfusion
 
 echo Create the new master package
 
-del %FULLPATH%\*.zip
+del %FULLPATH%*.zip
 
-7za a "%FULLPATH%\jfusion_package.zip" .\tmp\* -xr!*.svn* > NUL
+7za a "%FULLPATH%jfusion_package.zip" .\tmp\* -xr!*.svn* > NUL
 
-RMDIR "%FULLPATH%\tmp" /S /Q
+RMDIR "%FULLPATH%tmp" /S /Q
 
 echo Create a ZIP containing all files to allow for easy updates
 chdir %FULLPATH%
-7za a "%FULLPATH%\jfusion_files.zip" administrator components language modules plugins -r -xr!*.svn* > NUL
+7za a "%FULLPATH%jfusion_files.zip" administrator components language modules plugins -r -xr!*.svn* > NUL
 IF "%action%"=="1" goto end
 
 :end
@@ -182,15 +182,15 @@ endlocal & set "%1=%ut%" & goto :EOF
 
 	IF "%3" == "" SET XMLFILE=jfusion
 
-	md %FULLPATH%\tmppackage
+	md %FULLPATH%tmppackage
 
-	copy "%FULLPATH%\%TARGETPATH%" "%FULLPATH%\tmppackage" /V /Y > NUL
+	c:\windows\system32\xcopy /S /E /C /V /Y "%FULLPATH%%TARGETPATH%" "%FULLPATH%tmppackage" > NUL
 
-	call :CreateXml %FULLPATH%\tmppackage\%XMLFILE%
+	call :CreateXml %FULLPATH%tmppackage\%XMLFILE%
 
-	7za a "%FULLPATH%\%TARGETDEST%" %FULLPATH%\tmppackage\* -xr!*.svn* > NUL
+	7za a "%FULLPATH%%TARGETDEST%" %FULLPATH%tmppackage\* -xr!*.svn* > NUL
 
-	RMDIR "%FULLPATH%\tmppackage" /S /Q
+	RMDIR "%FULLPATH%tmppackage" /S /Q
 endlocal & goto :EOF
 
 :CreateXml
