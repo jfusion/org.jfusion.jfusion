5c05fa877b788adeb03c1ce3f08f38ad7a1b9104
fanno <morten@jfusion.org>
2013-03-24 11:43:59 +0100

    not sure why this file has not been merged ?

--- administrator/components/com_jfusion/models/model.frameless.php
+++ administrator/components/com_jfusion/models/model.frameless.php
@@ -19,9 +19,16 @@ require_once (JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_jfusion' . DS 
  * @package JFusion
  */
 
-class JFusionFrameless{
-
-	function initData($jname,$isPlugin=true)
+class JFusionFrameless {
+	/**
+	 * @static
+	 * @param $jname
+	 * @param bool $isPlugin
+	 *
+	 * @return \stdClass
+	 *
+	 */
+	public static function initData($jname,$isPlugin=true)
 	{
 		$uri = JURI::getInstance ();
 
@@ -29,6 +36,7 @@ class JFusionFrameless{
 		$data = new stdClass ( );
 		$data->buffer = null;
 		$data->header = null;
+		$data->bodyAttributes = null;
 		$data->body = null;
 		$data->baseURL = null;
 		$data->fullURL = null;
@@ -44,119 +52,149 @@ class JFusionFrameless{
 		$query = $uri->getQuery ();
 		$url = $uri->current ();
 		$data->fullURL = $query ? $url . '?' . $query : $url;
-        $data->fullURL = str_replace('&', '&amp;', $data->fullURL);
+		$data->fullURL = str_replace('&', '&amp;', $data->fullURL);
 
-        $menu = JSite::getMenu ();
-    	if(!$isPlugin) {
-            $item = $menu->getItem($jname);
+		/**
+		 * @ignore
+		 * @var $menu JMenu
+		 */
+		$menu = JSite::getMenu();
+		if(!$isPlugin) {
+			$item = $menu->getItem($jname);
 
-            if ($item) {
-            	$JFusionParam = $menu->getParams ( $item->id );
-            	$MenuParam = $menu->getParams ( $item->id );
-            } else {
-            	$JFusionParam = $menu->getParams ( null );
-            	$MenuParam = $menu->getParams ( null );
-            }
-    	} else {
-    		$MenuParam = $menu->getParams ( $data->Itemid );
-            $JFusionParam = JFusionFactory::getParams ( $jname );
-        }
-        
-        $data->jParam = $JFusionParam;
+			if ($item) {
+				$JFusionParam = $menu->getParams ( $item->id );
+				$MenuParam = $menu->getParams ( $item->id );
+			} else {
+				$JFusionParam = $menu->getParams( null );
+				$MenuParam = $menu->getParams( null );
+			}
+		} else {
+			$MenuParam = $menu->getParams ( $data->Itemid );
+			$JFusionParam = JFusionFactory::getParams ( $jname );
+		}
+
+		$data->jParam = $JFusionParam;
+		$data->mParam = $MenuParam;
+
+		$JFusionPluginParam = $MenuParam->get('JFusionPluginParam');
+		if ($JFusionPluginParam) {
+			$params = unserialize(base64_decode($JFusionPluginParam));
+			if ($params && isset($params['jfusionplugin'])) {
+				if (JFusionFunction::isJoomlaVersion('1.6') && isset($params[$params['jfusionplugin']]['params'])) {
+					$params += $params[$params['jfusionplugin']]['params'];
+					unset($params[$params['jfusionplugin']]);
+				}
+				$data->mParam->loadArray($params);
+			}
+		}
 
 		//Get the integrated URL
-        $data->integratedURL = $JFusionParam->get ( 'source_url' );
+		$data->integratedURL = $JFusionParam->get ( 'source_url' );
 
-        $data->source_url = $JFusionParam->get('source_url');
-        $data->cookie_domain = $JFusionParam->get('cookie_domain');
-        $data->cookie_path = $JFusionParam->get('cookie_path');
-        $data->cookie_expires = $JFusionParam->get('cookie_expires');
-        $data->httpauth = $JFusionParam->get('httpauth');
-        $data->httpauth_username = $JFusionParam->get('curl_username');
-        $data->httpauth_password = $JFusionParam->get('curl_password');
-        $data->verifyhost = $JFusionParam->get('verifyhost');
+		$data->source_url = $JFusionParam->get('source_url');
+		$data->cookie_domain = $JFusionParam->get('cookie_domain');
+		$data->cookie_path = $JFusionParam->get('cookie_path');
+		$data->cookie_expires = $JFusionParam->get('cookie_expires');
+		$data->httpauth = $JFusionParam->get('httpauth');
+		$data->httpauth_username = $JFusionParam->get('curl_username');
+		$data->httpauth_password = $JFusionParam->get('curl_password');
+		$data->verifyhost = $JFusionParam->get('verifyhost');
 
-		$data->jroute = $JFusionParam->get('sefmode',0);
+		$data->sefmode = $MenuParam->get('sefmode',$JFusionParam->get('sefmode',0));
 
-        $data->bodymap = $JFusionParam->get('bodymap');
-        $data->headermap = $JFusionParam->get('headermap');
+		$data->bodyextract = $JFusionParam->get('bodyextract');
+		$data->bodyremove = $JFusionParam->get('bodyremove');
 
-        $data->bodyextract = $JFusionParam->get('bodyextract');
-        $data->bodyremove = $JFusionParam->get('bodyremove');
+		// CSS PARSER INFO
+		$data->default_css = $MenuParam->get('default_css',1);
+		$data->default_css_overflow = $MenuParam->get('default_css_overflow' ,'visible');
 
-        // CSS PARSER INFO
-        $data->default_css = $MenuParam->get('default_css',1);
-        $data->default_css_overflow = $MenuParam->get('default_css_overflow' ,'visible');
-        
-        $data->parse_infile_css = $MenuParam->get('parse_infile_css',1);
-        $data->parse_css = $MenuParam->get('parse_css',1);
-        
-        $data->parse_anchors = $MenuParam->get('parse_anchors',1);
-        $data->parse_rel_url = $MenuParam->get('parse_rel_url',1);
-        $data->parse_abs_url = $MenuParam->get('parse_abs_url',1);
-        $data->parse_abs_path = $MenuParam->get('parse_abs_path',1);
-        $data->parse_rel_img = $MenuParam->get('parse_rel_img',1);
-        $data->parse_action = $MenuParam->get('parse_action',1);
-        $data->parse_popup = $MenuParam->get('parse_popup',1);
-        
-		//Load language files
-		$lang = & JFactory::getLanguage ();
-		$lang->load ( 'com_jfusion.plg_' . $jname );
+		$data->parse_infile_css = $MenuParam->get('parse_infile_css',1);
+		$data->parse_css = $MenuParam->get('parse_css',1);
+
+		$data->parse_anchors = $MenuParam->get('parse_anchors',$JFusionParam->get('parse_anchors',1));
+		$data->parse_rel_url = $MenuParam->get('parse_rel_url',$JFusionParam->get('parse_rel_url',1));
+		$data->parse_abs_url = $MenuParam->get('parse_abs_url',$JFusionParam->get('parse_abs_url',1));
+		$data->parse_abs_path = $MenuParam->get('parse_abs_path',$JFusionParam->get('parse_abs_path',1));
+		$data->parse_rel_img = $MenuParam->get('parse_rel_img',$JFusionParam->get('parse_rel_img',1));
+		$data->parse_action = $MenuParam->get('parse_action',$JFusionParam->get('parse_action',1));
+		$data->parse_popup = $MenuParam->get('parse_popup',$JFusionParam->get('parse_popup',1));
+
+		$data->bodymap = $JFusionParam->get('bodymap',$JFusionParam->get('bodymap'));
+		$data->headermap = $JFusionParam->get('headermap',$JFusionParam->get('headermap'));
+
 		return $data;
 	}
 
-	function displayContent($data)
+
+	/**
+	 * @static
+	 * @param $data
+	 * @return bool
+	 */
+	public static function displayContent($data)
 	{
 		$mainframe = JFactory::getApplication();
+		/**
+		 * @ignore
+		 * @var $document JDocumentHTML
+		 */
 		$document = JFactory::getDocument();
-		
+
 		if (!$data->isPlugin) {
 			require_once(JPATH_ADMINISTRATOR .DS.'components'.DS.'com_jfusion'.DS.'models'.DS.'model.abstractpublic.php');
-			$JFusionPlugin =& new JFusionPublic();
+			$JFusionPlugin = new JFusionPublic();
 		} else {
-			$JFusionPlugin =& JFusionFactory::getPublic ( $data->jname );
-			
+			$JFusionPlugin = JFusionFactory::getPublic ( $data->jname );
+
 			$sef_suffix = $mainframe->getCfg('sef_suffix');
 			$sef = $mainframe->getCfg('sef');
 			if($sef_suffix == 1 && $sef == 1 && !count(JRequest::get('POST'))){
-			    //redirect if url non_sef
-			    if (strrpos($data->fullURL, '?') !== false) {
-			        $u =& JFactory::getURI();
-			        if ($u->getVar('Itemid') && $u->getVar('option')) {
-			            $u->delVar('Itemid');
-			            $u->delVar('option');
-			            $jfile = $u->getVar('jfile');
-			            if ($jfile) {
-			                $u->delVar('jfile');
-			            }
-			            $url = $u->getQuery();
-			            $url = JFusionFunction::routeURL($jfile.'?'.$url,$data->Itemid,'',true,false);
-			            $mainframe->redirect($url);
-			        }
-			    }
-			}			
-			
-            /*
+				//redirect if url non_sef
+				if (strrpos($data->fullURL, '?') !== false) {
+					$u = JFactory::getURI();
+					if ($u->getVar('Itemid') && $u->getVar('option')) {
+						$u->delVar('Itemid');
+						$u->delVar('option');
+						$jfile = $u->getVar('jfile');
+						if ($jfile) {
+							$u->delVar('jfile');
+						}
+						$url = $u->getQuery();
+						$url = JFusionFunction::routeURL($jfile.'?'.$url,$data->Itemid,'',true,false);
+						$mainframe->redirect($url);
+					}
+				}
+			}
+
+			/*
 			 * Caused issues with more people than it helped
-            //make sure that the software's database is selected in the case the mysql server and credentials are the same but a different database is used
-            $JFusionParam =& JFusionFactory::getParams($data->jname);
-            $db_name = $JFusionParam->get('database_name');
-            if (!empty($db_name)) {
-            	$db =& JFusionFactory::getDatabase($this->jname);
-                $query = "USE $db_name";
-                $db->Execute($query);
-            }
-            */
+			//make sure that the software's database is selected in the case the mysql server and credentials are the same but a different database is used
+			$JFusionParam = JFusionFactory::getParams($data->jname);
+			$db_name = $JFusionParam->get('database_name');
+			if (!empty($db_name)) {
+				$db = JFusionFactory::getDatabase($this->jname);
+				$query = 'USE '.$db_name;
+				$db->setQuery($query);
+				$db->query();
+			}
+			*/
 		}
 
-		//get Joomla's session token so we can reset it afterward in case the software closes the session
-        $session = JFactory::getSession();
-        $token = $session->getToken();
+		//get Joomla session token so we can reset it afterward in case the software closes the session
+		$session = JFactory::getSession();
+		$token = $session->getToken();
 
+		$REQUEST = $_REQUEST; // backup variables
+
+		$JFusionPlugin->data = $data;
 		$JFusionPlugin->getBuffer ( $data );
 
-        //restore session token
-        $session->set('session.token', $token);
+		$_REQUEST = $REQUEST; // restore backup
+
+		//restore session token
+		$session->set('session.token', $token);
 
 		//clear the page title
 		if (! empty ( $data->buffer )) {
@@ -167,8 +205,8 @@ class JFusionFrameless{
 			}
 		}
 
-		//check to see if the Joomla database is still connnected incase the plugin messed it up
-		JFusionFunction::reconnectJoomlaDb ();
+		//check to see if the Joomla database is still connected in case the plugin messed it up
+		JFusionFunction::reconnectJoomlaDb();
 
 		if ($data->buffer === 0) {
 			JError::raiseWarning ( 500, JText::_ ( 'NO_FRAMELESS' ) );
@@ -212,17 +250,17 @@ class JFusionFrameless{
 				//change the page title
 				$pattern = '#<title>(.*?)<\/title>#si';
 				preg_match ( $pattern, $data->header, $page_title );
-				
+
 				if (JFusionFunction::isJoomlaVersion('1.6')) {
 					$document->setTitle( html_entity_decode ( $page_title [1], ENT_QUOTES, "utf-8" ) );
 				} else {
 					$mainframe->setPageTitle ( html_entity_decode ( $page_title [1], ENT_QUOTES, "utf-8" ) );
 				}
-				
+
 				$regex_header [] = $pattern;
 				$replace_header [] = '';
 
-				//set meta data to that of softwares
+				//set meta data to that of software
 				$meta = array ('keywords', 'description', 'robots' );
 
 				foreach ( $meta as $m ) {
@@ -245,7 +283,7 @@ class JFusionFrameless{
 					$replace_header [] = '';
 				}
 
-				//use Joomla's default
+				//use Joomla default
 				$regex_header [] = '#<meta http-equiv=["|\']Content-Type["|\'](.*?)>#Si';
 				$replace_header [] = '';
 
@@ -289,6 +327,10 @@ class JFusionFrameless{
 		return true;
 	}
 
+	/**
+	 * @param $buffer
+	 * @return string
+	 */
 	function parseEncoding($buffer) {
 		if ( preg_match  ( '#<meta.*?content="(.*?); charset=(.*?)".*?/>#isS'  , $buffer , $matches)) {
 			if ( stripos  ( $matches[1] , 'text/html' ) !== false && stripos( $matches[2] , 'utf-8' ) === false ) {
@@ -298,8 +340,13 @@ class JFusionFrameless{
 		return $buffer;
 	}
 
+	/**
+	 * @param $data
+	 * @return mixed
+	 */
 	function parseBody(&$data) {
 		if ( !empty($data->bodyextract) || !empty($data->bodyremove) ) {
+			/*
 			require_once (JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_jfusion' . DS . 'models' . DS . 'parsers' . DS . 'simple_html_dom.php');
 			$html = str_get_html($data->body);
 
@@ -321,13 +368,14 @@ class JFusionFrameless{
 					$elements = $html->find(trim($value));
 					if ( $elements ) {
 						foreach( $elements as $element ) {
-							$data->body = $element->outertext;
+							$data->body = $element->outertext();
 							return;
 						}
 					}
 				}
 			}
-			$data->body = $html->outertext;
+			$data->body = $html->outertext();
+			*/
 		}
 	}
 }
