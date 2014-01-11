1c28a23bba1d0dfb3803a5c9f7534332f0d4c1c4
Morten Hundevad <morten@jfusion.org>
2014-01-11 13:31:02 +0100

    fixed time offset ?

--- components/com_jfusion/plugins/vbulletin/user.php
+++ components/com_jfusion/plugins/vbulletin/user.php
@@ -629,9 +629,15 @@ class JFusionUser_vbulletin extends JFusionUser
 
             //set the timezone
             if (!isset($userinfo->timezone)) {
-                $config = JFactory::getConfig();
-                $userinfo->timezone = $config->getValue('config.offset',0);
+	            $mainframe = JFactory::getApplication();
+                $userinfo->timezone = $mainframe->getCfg('offset', 0);
             }
+
+	        if (!is_numeric($userinfo->timezone)) {
+		        $timezone = new DateTimeZone($userinfo->timezone);
+		        $offset = $timezone->getOffset(new DateTime('NOW'));
+		        $userinfo->timezone = $offset/3600;
+	        }
 
             $apidata['userinfo'] = $userinfo;
 
