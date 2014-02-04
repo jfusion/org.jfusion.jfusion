cd243d60e2a71567f812ba6554ff7aeb53823721
Morten Hundevad <morten@jfusion.org>
2014-02-04 18:32:42 +0100

    dokuwiki fix link for #......

--- components/com_jfusion/plugins/dokuwiki/public.php
+++ components/com_jfusion/plugins/dokuwiki/public.php
@@ -260,7 +260,7 @@ class JFusionPublic_dokuwiki extends JFusionPublic {
         $q = urldecode($q);
         $q = str_replace(':', ';', $q);
         if (strpos($q, '#') === 0) {
-            $url = $q;
+	        $url = $fullURL . $q;
         } else {
             $q = ltrim($q, '/');
             if (strpos($q, '_detail/') === 0 || strpos($q, 'lib/exe/detail.php') === 0) {
