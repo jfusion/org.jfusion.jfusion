675a8a4bf32ffe4cd9906a6a4ad893afb9a63af0
Morten Hundevad <morten@jfusion.org>
2014-07-29 10:15:47 +0200

    fixed: common.php missing.

--- components/com_jfusion/plugins/phpbb3/user.php
+++ components/com_jfusion/plugins/phpbb3/user.php
@@ -212,7 +212,7 @@ class User extends Plugin_User
 				    if ($login_type != 1 && !function_exists('deregister_globals')) {
 					    //let phpbb3 handle login
 					    $source_path = $this->params->get('source_path');
-					    if (!file_exists($source_path . 'common.php')) {
+					    if (file_exists($source_path . 'common.php')) {
 						    //set the current directory to phpBB3
 						    chdir($source_path);
 						    /* set scope for variables required later */
