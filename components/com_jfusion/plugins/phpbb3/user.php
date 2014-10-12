c9d15cf84a2f21bc83073ee7acf61d62b1c66bb0
Morten Hundevad <morten@jfusion.org>
2014-10-12 22:42:50 +0200

    version change for security update

--- components/com_jfusion/plugins/phpbb3/user.php
+++ components/com_jfusion/plugins/phpbb3/user.php
@@ -734,7 +734,7 @@ class JFusionUser_phpbb3 extends JFusionUser
 			    $query = $db->getQuery(true)
 				    ->select('user_id as userid, username as username')
 				    ->from('#__users')
-				    ->where('username_clear = ' . $db->quote($username_clean));
+				    ->where('username_clean = ' . $db->quote($username_clean));
 
 			    $db->setQuery($query);
 			    $result = $db->loadObject();
