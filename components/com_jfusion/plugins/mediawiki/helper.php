fdd9d6d2af4d896dd40632061a37ad1614463731
Morten Hundevad <morten@jfusion.org>
2014-12-23 02:55:45 +0100

    Changed: framework loader for new version of mediawiki

--- components/com_jfusion/plugins/mediawiki/helper.php
+++ components/com_jfusion/plugins/mediawiki/helper.php
@@ -123,17 +123,31 @@ class JFusionHelper_mediawiki extends JFusionPlugin
         $return[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'DefaultSettings.php';
         $return[] = $source_path . 'LocalSettings.php';
 
+        global $IP, $wgStylePath;
+        $IP = $source_path;
+
+        $source_url = $this->params->get('source_url');
+
+        $uri = new JURI($source_url);
+
+        $wgStylePath = $uri->getPath() .'skinns';
+
+//        $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'WebStart.php';
+
+        $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'AutoLoader.php';
         $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'Defines.php';
         $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'IP.php';
-	    $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'IP.php';
+        $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'IP.php';
         $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'WebRequest.php';
         $paths[] = $source_path . 'includes'. DIRECTORY_SEPARATOR . 'SiteConfiguration.php';
+
         defined ('MEDIAWIKI') or define('MEDIAWIKI', TRUE);
         defined ('MW_INSTALL_PATH') or define('MW_INSTALL_PATH', $source_path);
+        putenv('MW_INSTALL_PATH=' . $source_path);
         foreach($paths as $path) {
-	        if (file_exists($path)) {
-		        include_once($path);
-	        }
+            if (file_exists($path)) {
+                include_once($path);
+            }
         }
         return $return;
     }
