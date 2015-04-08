658a91abb782205f77b35bee977783b534ad462c
Morten Hundevad <morten@jfusion.org>
2015-04-08 13:06:25 +0200

    incorrect action set.

--- administrator/components/com_jfusion/libraries/jfusion/plugin/plugin/user.php
+++ administrator/components/com_jfusion/libraries/jfusion/plugin/plugin/user.php
@@ -600,7 +600,7 @@ class JFusionUser extends JFusionPlugin
 		if ((empty($create_inactive) && !empty($userinfo->activation)) || (empty($create_blocked) && !empty($userinfo->block))) {
 			//block user creation
 			$this->debugger->add('debug', JText::_('SKIPPED_USER_CREATION'));
-			$this->debugger->set('debug', 'unchanged');
+			$this->debugger->set('action', 'unchanged');
 			$this->debugger->set('userinfo', null);
 		} else {
 			$this->debugger->add('debug', JText::_('NO_USER_FOUND_CREATING_ONE'));
